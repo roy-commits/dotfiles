bea7620104f2f4a53f63a9f00f5f912d67cff148
roy <_roy_@kernel.com>
2025-02-25 21:47:02 +0800

    nvim config optimize

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,9 @@
 ---@type LazySpec
 return {
+  { -- Override `nvim-tree` plugin
+    "nvim-tree/nvim-tree.lua",
+    opts = require("configs.ui").nvim_tree,
+  },
   { -- Breadcrumbs
     "Bekaboo/dropbar.nvim",
     lazy = false,
